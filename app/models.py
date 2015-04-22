b139e518d7ae5313a53a140ce44b349bd98ffdf4
chet <chet@chets-MacBook-Air.local>
2015-04-22 13:20:37 -0700

    db setup

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from app import db
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
 
-from app import views, models
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
