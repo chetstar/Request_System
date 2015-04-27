7bf51bc2a131642da23a6d9e798c0c32732444cb
Chet <chetstar@gmail.com>
2015-04-27 16:08:53 -0700

    not giving access

--- app/views.py
+++ app/views.py
@@ -1,41 +1,38 @@
-from flask import render_template, flash, redirect,Flask,Response,request,url_for
+from flask import render_template, flash, redirect,Flask,Response,request,url_for, g
 from app import app
-from flask.ext.login import LoginManager, UserMixin, login_required
+from flask.ext.login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 from forms import LoginForm, validators
 import ldap
 
+
 login_manager = LoginManager()
 login_manager.init_app(app) 
-
+# login_manager.session_protection = None
+#login_managerlogin_view = 'login'
 
 class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-               "JaneDoe": ("JaneDoe", "Jane")}
+  def __init__(self, name, id, active=True):
+    self.name = name
+    self.id = id
+    self.active = active 
 
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
+  def is_authenticated(self):
+      return True
 
-    @classmethod
-    def get(cls,id):
-        return cls.user_database.get(id)
+  def is_active(self):
+    return self.active   
+
+USERS = {
+'\x92\xbc\xe1\x9d\xf2\x03\x96K\x9d\xbb\xe7\x91\x1f\x07N\x86': User(u"Notch", 1),
+2: User(u"Steve", 2),
+4: User(u"Stevex", 4),
+3: User(u"Creeper", 3, False),
+} 
 
 
-@login_manager.request_loader
-def load_user(request):
-    token = request.headers.get('Authorization')
-    if token is None:
-        token = request.args.get('token')
-
-    if token is not None:
-        username,password = token.split(":") # naive token
-        user_entry = User.get(username)
-        if (user_entry is not None):
-            user = User(user_entry[0],user_entry[1])
-            if (user.password == password):
-                return user
-    return None
+@login_manager.user_loader
+def load_user(id):
+  return USERS.get(id) 
 
 
 @app.route("/",methods=["GET"])
@@ -55,13 +52,15 @@ def login():
         l = ldap.initialize("ldap://10.129.18.101")
         l.simple_bind_s("program\%s" % form.username.data,form.password.data)
         print "Authentification Successful"
-        r=l.search_s('cn=Users,dc=BHCS,dc=Internal',ldap.SCOPE_SUBTREE,'(sAMAccountName=*%s*)' % form.username.data,['mail'])
-        email=r[0][1]['mail']        
-        # login_user(user)
+        r=l.search_s('cn=Users,dc=BHCS,dc=Internal',ldap.SCOPE_SUBTREE,'(sAMAccountName=*%s*)' % form.username.data,['mail','objectGUID'])
+        email=r[0][1]['mail'][0]   
+        GUID=r[0][1]['objectGUID'][0]     
+        import pdb;pdb.set_trace()
+        login_user(load_user(2),remember=True)
         flash("Logged in successfully.")
-        return redirect(request.args.get("next") or url_for("index"))
+        return redirect(request.args.get("next") or url_for("protected"))
     return render_template("login.html", form=form)
-
+# USERS.get('\x92\xbc\xe1\x9d\xf2\x03\x96K\x9d\xbb\xe7\x91\x1f\x07N\x86')
 # @app.route('/')
 # def index():
 #     user = {'nickname': 'Chet'}  # fake user
